73da223f0e3d03146285aad7d49afa09db8341d5
abhijeet4725 <abhijeetcs4725@gmail.com>
2025-09-30 01:47:13 +0530

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/abhijeet/outbreakAI/OutbreakAiApplication.java
@@ -0,0 +1,13 @@
+package com.abhijeet.outbreakAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutbreakAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutbreakAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhijeet/outbreakAI/OutbreakAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijeet.outbreakAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutbreakAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
